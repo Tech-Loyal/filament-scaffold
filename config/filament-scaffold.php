f16657976680c34472baf501eb371d4ff755a191
Tech-Loyal <103830225+Tech-Loyal@users.noreply.github.com>
2026-04-29 10:47:59 -0500

    Change scaffold enabled setting to use environment variable
    Updated the 'enabled' configuration to use an environment variable for better flexibility.

--- config/filament-scaffold.php
+++ config/filament-scaffold.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-    'enabled' => true,
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Filament Scaffold
+    |--------------------------------------------------------------------------
+    | Set to true to enable the Scaffold Manager in your Filament panel.
+    | It is recommended to disable this in production environments.
+    |
+    */
+    'enabled' => env('FILAMENT_SCAFFOLD_ENABLED', true),
 ];
